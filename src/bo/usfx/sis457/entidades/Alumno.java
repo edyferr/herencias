5f9c62f75c708a18b94f3551450bda62e764bb58
EdgarMostacedo <edyfer_mt_@hotmail.com>
2018-05-11 19:44:15 -0400

    modificando la clase alumno

--- src/bo/usfx/sis457/entidades/Alumno.java
+++ src/bo/usfx/sis457/entidades/Alumno.java
@@ -11,50 +11,66 @@ import java.util.GregorianCalendar;
 
 /**
  *
- * @author Marcelo
+ * añadirle  la propiedad Asignaturas que contenga los nombres de las asignaturas programadas (tipo arreglo de cadenas).
  */
 public class Alumno extends Persona {
     protected String CarnetUniversitario;
     protected int Semestre;
-    
-    public Alumno(String carnetUniversitario, int semestre, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
+      protected String Asignaturas;
+    private String Asignatura;
+
+    public Alumno(String CarnetUniversitario, int Semestre, String Asignaturas, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
         super(carnetIdentidad, nombre, fechaNacimiento);
-        this.CarnetUniversitario = carnetUniversitario;
-        this.Semestre = semestre;
-        //System.out.println("Se crea un Alumno");
+        this.CarnetUniversitario = CarnetUniversitario;
+        this.Semestre = Semestre;
+        this.Asignaturas = Asignaturas;
     }
-    
-    public Alumno(String carnetUniversitario, int semestre) {
-        super();
-        this.CarnetUniversitario = carnetUniversitario;
-        this.Semestre = semestre;
-        //System.out.println("Se crea un Alumno");
+
+    public Alumno(String CarnetUniversitario, int Semestre, String Asignaturas) {
+        this.CarnetUniversitario = CarnetUniversitario;
+        this.Semestre = Semestre;
+        this.Asignaturas = Asignaturas;
     }
-    
+
     public String getCarnetUniversitario() {
         return CarnetUniversitario;
     }
 
-    public void setCarnetUniversitario(String carnetUniversitario) {
-        this.CarnetUniversitario = carnetUniversitario;
+    public void setCarnetUniversitario(String CarnetUniversitario) {
+        this.CarnetUniversitario = CarnetUniversitario;
     }
 
     public int getSemestre() {
         return Semestre;
     }
 
-    public void setSemestre(int semestre) {
-        this.Semestre = semestre;
+    public void setSemestre(int Semestre) {
+        this.Semestre = Semestre;
     }
 
+    public String getAsignaturas() {
+        return Asignaturas;
+    }
+
+    public void setAsignaturas(String Asignaturas) {
+        this.Asignaturas = Asignaturas;
+    }
+    
+    
+   
     @Override
     public String toString() {
         return "Alumno{" 
                 + "Id=" + this.Id 
                 + ", CarnetIdentidad=" + this.CarnetIdentidad 
-                + ", Nombre=" + this.Nombre 
+                + ", Nombre=" + this.Nombre
+                + ", Asignaturas=" + this.Asignatura
                 + ", FechaNacimiento=" + Utilitarios.getFechaCalendario(this.FechaNacimiento)
                 + ", CarnetUniversitario=" + this.CarnetUniversitario 
                 + ", Semestre=" + this.Semestre + '}';
     }
 }
+
+
+
+ 
