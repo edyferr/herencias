54448052d54228626eab897276556d122ddbbc7b
EdgarMostacedo <edyfer_mt_@hotmail.com>
2018-05-11 17:45:47 -0400

    agregando la clase adminnistrativo

--- src/bo/usfx/sis457/entidades/Administrativo.java
+++ src/bo/usfx/sis457/entidades/Administrativo.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bo.usfx.sis457.entidades;
+
+import bo.usfx.sis457.Utilitarios;
+import java.util.Calendar;
 
 /**
  *
- * @author Marcelo
+ * cargo, lugar de trabajo y fecha de ingreso (Cargo:String, LugarTrabajo:String y FechaIngreso:Calendar).
  */
-public class Administrativo {
+public class Administrativo extends Persona {
+    protected String Cargo;
+    protected String LugarTabejo;
+    protected Calendar FechaIngreso;
+
+    public Administrativo(String Cargo, String LugarTabejo, Calendar FechaIngreso, String carnetIdentidad, String nombre, Calendar fechaNacimiento) {
+        super(carnetIdentidad, nombre, fechaNacimiento);
+        this.Cargo = Cargo;
+        this.LugarTabejo = LugarTabejo;
+        this.FechaIngreso = FechaIngreso;
+    }
+
+    public Administrativo(String Cargo, String LugarTabejo, Calendar FechaIngreso) {
+        this.Cargo = Cargo;
+        this.LugarTabejo = LugarTabejo;
+        this.FechaIngreso = FechaIngreso;
+    }
+
+    public Administrativo() {
+        
+    }
+
+    public String getCargo() {
+        return Cargo;
+    }
+
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+
+    public String getLugarTabejo() {
+        return LugarTabejo;
+    }
+
+    public void setLugarTabejo(String LugarTabejo) {
+        this.LugarTabejo = LugarTabejo;
+    }
+
+    public Calendar getFechaIngreso() {
+        return FechaIngreso;
+    }
+
+    public void setFechaIngreso(Calendar FechaIngreso) {
+        this.FechaIngreso = FechaIngreso;
+    }
     
-}
+    
+   
+    
+    public String toString() {
+        return "Administrativo{" 
+                + "Id=" + this.Id 
+                + ", CarnetIdentidad=" + this.CarnetIdentidad 
+                + ", Nombre=" + this.Nombre 
+                + ", FechaNacimiento=" + Utilitarios.getFechaCalendario(this.FechaNacimiento)
+                + ", Cargo=" + Cargo + ", LugarTabejo=" + LugarTabejo + '}';
+    }
+    
+}                           
